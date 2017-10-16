8505ddc8e675dd1600576607532b8e99b4141df5
enmisac <enmisac@gmail.com>
2017-10-16 02:03:49 -0600

    se agregue archivo de autenticacion y se logro implementar el metodo de autetncacion local

---
+++ auth/index.js
@@ -0,0 +1,36 @@
+var LocalStrategy = require('passport-local').Strategy;
+var platzigram = require('platzigram-client');
+var config = require('../config');
+
+var client = platzigram.createClient(config.client);
+
+exports.localStrategy = new LocalStrategy(function (username, password, done) {
+	client.auth(username, password, (err, token) => {
+		if (err) {
+			return done(null, false, { message: 'username and password not found' });
+		}
+
+		client.getUser(username, (err, user) => {
+			if (err) {
+				return done(null, false, { message: `an error courred: ${err.message}` });
+			}
+
+			user.token = token;
+			return done(null, user);
+		});
+	});
+});
+
+exports.serializeUser = function (user, done) {
+	done(null, {
+		username: user.username,
+		token: user.token
+	});
+}
+
+exports.deserializeUser = function (user, done) {
+	client.getUser(user.username, function (err, usr) {
+		usr.token = user.token;
+		done(err, usr);
+	});
+}
