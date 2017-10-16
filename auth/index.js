3f3a594dc3482826d07cc522a517c31afe55fbe0
enmisac <enmisac@gmail.com>
2017-10-16 03:01:30 -0600

    se creó la estrategia de facebook así como su aplicacion en el portal de desarrolladores de facebook

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,5 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-faceboook').Strategy;
 var platzigram = require('platzigram-client');
 var config = require('../config');
 
@@ -19,6 +20,34 @@ exports.localStrategy = new LocalStrategy(function (username, password, done) {
 			return done(null, user);
 		});
 	});
+});
+
+exports.facebookStrategy = new FacebookStrategy({
+	clientID: config.auth.facebook.clientID,
+	clientSecret: config.auth.facebook.clientSecret,
+	callbackURL: config.auth.facebook.callbackURL,
+	profileFields: ['id', 'displayName', 'email']
+}, function (accessToken, refreshToken, profile, done) {
+	var userProfile = {
+		username: profile._json.id,
+		name: profile._json.name,
+		email: profile._json.email,
+		facebook: true
+	};
+
+	findOrCreate(userProfile, function (err, user) {
+		return done(null, user)
+	})
+
+	function findOrCreate (user, done) {
+		client.getUser(user.username, function (err, usr) {
+			if (err) {
+				return client.saveUser(user, callback);
+			}
+
+			callback(null, usr)
+		})
+	}
 });
 
 exports.serializeUser = function (user, done) {
